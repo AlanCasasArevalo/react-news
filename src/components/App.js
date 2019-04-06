3a68ffbd78e51dd090497933f11fc3e3f4726c8b
Alan Casas <alancasasarevalo@gmail.com>
2019-04-06 20:45:20 +0200

    [FEATURE] [API URL] - Url was added to attack news api

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from "./Header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header
-            title="Noticias"
-          />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.getTopHeadlines();
+    }
+
+    getTopHeadlines = () => {
+        const apiKey = 'YOUR_API_KEY';
+        const category = 'general';
+        const country = 'co';
+        const urlNews = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`
+
+        console.log("URL A ATACAR", urlNews);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header
+                    title="Noticias"
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
