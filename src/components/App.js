f3b4a0775198cb5b6c759ef2109320b66faceab9
Alan Casas <alancasasarevalo@gmail.com>
2019-04-06 21:39:41 +0200

    [FEATURE] [NEWS] - All news were received from api and set into state

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,35 @@ import React, {Component} from 'react';
 import Header from "./Header";
 
 class App extends Component {
+
+    state = {
+        news: []
+    }
+
     componentDidMount() {
         this.getTopHeadlines();
     }
 
     getTopHeadlines = () => {
-        const apiKey = 'YOUR_API_KEY';
         const category = 'general';
         const country = 'co';
         const urlNews = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`
 
         console.log("URL A ATACAR", urlNews);
+        fetch(urlNews)
+            .then(response => {
+                return response.json()
+            })
+            .then(allNewsFromApi => {
+                console.log("all news", allNewsFromApi.articles);
+                this.setState({
+                    news: allNewsFromApi.articles 
+                })
+            })
+            .catch(error => {
+
+            })
+
     };
 
     render() {
